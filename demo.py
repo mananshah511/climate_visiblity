781cbeb0752c736fc29470889a100e8d998a3355
Manan <manan511shah@gmail.com>
2023-10-22 16:15:04 +0530

    Data Validation Stage added and tested

--- demo.py
+++ demo.py
@@ -1,8 +1,8 @@
 from climate.config.configuration import Configuration
 from climate.pipeline.pipeline import Pipeline
 
-config = Configuration()
-config.get_data_validation_config()
+#config = Configuration()
+#config.get_data_validation_config()
 
-#pipeline = Pipeline()
-#pipeline.run_pipeline()
+pipeline = Pipeline()
+pipeline.run_pipeline()
