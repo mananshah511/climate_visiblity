d0437cc948dd442af9478fa1cf18c36a21a814a7
Manan <manan511shah@gmail.com>
2023-10-22 18:45:33 +0530

    Model Evulation Stage added and tested

--- climate/pipeline/pipeline.py
+++ climate/pipeline/pipeline.py
@@ -2,11 +2,12 @@ import os,sys
 from climate.logger import logging
 from climate.exception import ClimateException
 from climate.config.configuration import Configuration
-from climate.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformArtifact,ModelTrainerArtifact
+from climate.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact,DataTransformArtifact,ModelTrainerArtifact,ModelEvulationArtifact
 from climate.components.data_ingestion import DataIngestion
 from climate.components.data_validation import DataValidation
 from climate.components.data_transform import DataTransform
 from climate.components.model_trainer import ModelTrainer
+from climate.components.model_evulation import ModelEvulation
 
 class Pipeline:
 
@@ -49,6 +50,16 @@ class Pipeline:
         except Exception as e:
             raise ClimateException(sys,e) from e
         
+    def start_model_evulation(self,data_transform_artifact:DataTransformArtifact,
+                              model_trainer_artifact:ModelTrainerArtifact)->ModelEvulationArtifact:
+        try:
+            model_evulation = ModelEvulation(data_transform_artifact=data_transform_artifact,
+                                             model_trainer_artifact=model_trainer_artifact,
+                                             model_evulation_config=self.config.get_model_evulation_config())
+            return model_evulation.intiate_model_evulation()
+        except Exception as e:
+            raise ClimateException(sys,e) from e
+        
     def run_pipeline(self):
         try:
             data_ingestion_artifact = self.start_data_ingestion()
@@ -56,5 +67,8 @@ class Pipeline:
             data_transform_artifact = self.start_data_transform(data_ingestion_artifact=data_ingestion_artifact,
                                                                 data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transform_artifact=data_transform_artifact)
+            model_evulation_artifact = self.start_model_evulation(data_transform_artifact=data_transform_artifact,
+                                                                  model_trainer_artifact=model_trainer_artifact)
+            
         except Exception as e:
             raise ClimateException(sys,e) from e
--- climate/util/util.py
+++ climate/util/util.py
@@ -1,4 +1,4 @@
-import os,sys,yaml
+import os,sys,yaml,dill
 from climate.logger import logging
 from climate.exception import ClimateException
 
@@ -6,5 +6,22 @@ def read_yaml(file_path:str):
     try:
         with open(file_path,'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise ClimateException(sys,e) from e
+    
+def write_yaml_file(file_path:str, data:dict=None):
+    try:
+        file_dir=os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        
+        with open(file_path,"w") as yaml_file:
+            yaml.dump(data, yaml_file)
+    except Exception as e:
+        raise ClimateException(sys,e) from e  
+    
+def load_object(file_path:str):
+    try:
+        with open(file_path,"rb") as object_file:
+            return dill.load(object_file)
     except Exception as e:
         raise ClimateException(sys,e) from e
